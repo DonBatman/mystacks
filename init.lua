b61f00fb0e130097cbce350cc85dcedbde945c21
Don <serfdon@gmail.com>
2025-03-24 18:03:00 -0700

    Update

--- init.lua
+++ init.lua
@@ -16,13 +16,39 @@ local stackitems = {"default:cobble",
 					"default:desert_stonebrick",
 					"default:sandstonebrick",
 					"default:desert_sandstone_brick",
-					"default:silver_sandstone_brick",
+					"default:silver_sandstone_brick"
 					}
+
+if minetest.get_modpath("mystreets") then
+	table.insert(stackitems,"mystreets:asphalt")
+	table.insert(stackitems,"mystreets:asphalt_center_dashed")
+	table.insert(stackitems,"mystreets:asphalt_center_solid")
+	table.insert(stackitems,"mystreets:asphalt_center_solid_dashed")
+	table.insert(stackitems,"mystreets:asphalt_center_solid_double")
+	table.insert(stackitems,"mystreets:asphalt_side_solid")
+end
+if minetest.get_modpath("myconcrete") then
+	table.insert(stackitems,"myconcrete:concrete")
+	table.insert(stackitems,"myconcrete:sidewalk")
+end
+
+
 for i, v in ipairs(stackitems) do
 	item = stackitems[i]
-
 minetest.override_item(item,{
 	stack_max = stack,
 })
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
